dc747611c9b2c131dca2bc89f3c94761f56e76c1
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-11 22:47:33 +0300

    add

--- Shoe/store/views.py
+++ Shoe/store/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
 from django import forms
 
 # Create your views here.
