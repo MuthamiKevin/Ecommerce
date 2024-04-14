696266be6a59817175382dddc4400f040d118257
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-14 16:30:00 +0300

    updates

--- Shoe/cart/views.py
+++ Shoe/cart/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
+
 
 def cart_summary(request):
     return render(request, "cart_summary.html", {})
 def cart_add(request):
-    pass
+    cart =  Cart(request, "cart_summary.html", {})
+    if request.POST.get('action') =='post':
+        product_id = int(request.POST.get('product_id'))
+        product = get_object_or_404(Product, id=product_id)
 def cart_delete(request):
     pass
 def cart_update(request):
