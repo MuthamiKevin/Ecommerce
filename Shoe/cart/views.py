b94bf44d6d9fa5bd823b80ed28161af5c9a2d104
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-14 19:24:04 +0300

    update

--- Shoe/cart/views.py
+++ Shoe/cart/views.py
@@ -7,10 +7,12 @@ from django.http import JsonResponse
 def cart_summary(request):
     return render(request, "cart_summary.html", {})
 def cart_add(request):
-    cart =  Cart(request, "cart_summary.html", {})
+    cart =  Cart(request)
     if request.POST.get('action') =='post':
         product_id = int(request.POST.get('product_id'))
+        #save to session
         product = get_object_or_404(Product, id=product_id)
+        
         cart.add(product=product)
         
         response = JsonResponse({'Product Name: ': product.name})
