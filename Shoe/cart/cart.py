35f0981fea81d05a3bbd371d4d1ff7ff147a262f
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-15 23:06:37 +0300

    update

--- Shoe/cart/cart.py
+++ Shoe/cart/cart.py
@@ -10,7 +10,7 @@ class Cart():
         
         self.cart = cart
         
-    def ad(self, product):
+    def add(self, product):
         product_id = str(product.id)
         
         if product_id in self.cart:
@@ -18,3 +18,6 @@ class Cart():
         else:
             self.cart[product_id] = {'price': str(product.price)}
             self.session.modified = True
+            
+    def __len__(self):
+        return len(self.cart)
