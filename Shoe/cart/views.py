94ee1f206d9e5d74c68ff9fd4add8eed976e29b5
Kevin Muthami <muthamikevin80@gmail.com>
2024-04-16 19:13:07 +0300

    update

--- Shoe/cart/views.py
+++ Shoe/cart/views.py
@@ -6,6 +6,7 @@ from django.http import JsonResponse, HttpResponseBadRequest
 
 def cart_summary(request):
     return render(request, "cart_summary.html", {})
+
 def cart_add(request):
     cart = Cart(request)
     if request.POST.get('action') == 'post':
